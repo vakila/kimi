6d77fff3ba60a1703ea0fbd7f8233b161065c885
Anjana <anjanav@coli.uni-saarland.de>
2015-11-17 13:22:55 -0500

    Start working on environment.py

---
+++ environment.py
@@ -0,0 +1,36 @@
+# Kimi language interpreter in Python 3
+# Anjana Vakil
+# http://www.github.com/vakila/kimi
+
+def standard_env():
+    '''Returns the standard environment as a dictionary of (variable: value) pairs
+    '''
+    env = dict()
+
+    env = add_booleans(env)
+
+    env = add_arithmentic(env)
+
+    env = add_logic(env)
+
+    env = add_comparison(env)
+
+    env = add_strings(env)
+
+    return env
+
+
+def add_booleans(env):
+    return env
+
+def add_arithmentic(env):
+    return env
+
+def add_logic(env):
+    return env
+
+def add_comparison(env):
+    return env
+
+def add_strings(env):
+    return env
